98acf22e92041291d4de07791d1e37eaaab78e00
Jacques Camara <jacquescamara02@email.com>
2026-07-21 12:23:19 +0400

    Retire mobile-dist du suivi git (artefact de build)

--- mobile-dist/server/assets/minus-Bc1ChSDm.js
+++
@@ -1,6 +0,0 @@
-import { t as createLucideIcon } from "./router-pZ3dyLDZ.js";
-const __iconNode = [["path", { d: "M5 12h14", key: "1ays0h" }]];
-const Minus = createLucideIcon("minus", __iconNode);
-export {
-  Minus as M
-};
--- mobile-dist/server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-mJJQyuR5.js";
-import "node:events";
-export {
-  w as default
-};
